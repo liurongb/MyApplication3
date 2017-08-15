760ec5bab63cb2ddf8314d8bb838d9ec34da42fa
liurongb <1270969067@qq.com>
2017-08-15 10:09:10 +0800

    子分支1 新增了log 2017-8-15 10:09:04

--- app/src/main/java/com/jiyun/lenovo/day03lx1/MainActivity.java
+++ app/src/main/java/com/jiyun/lenovo/day03lx1/MainActivity.java
@@ -1,7 +1,8 @@
 package com.jiyun.lenovo.day03lx1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("TAG","子分支");
     }
 }
